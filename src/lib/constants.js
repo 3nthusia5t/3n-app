e6ff9bc97525f11aa1a2544eeba5218cb51704b2
3nthusia5t <3nthusiast@protonmail.com>
2024-04-30 00:04:38 +0200

    update the dev variables

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -3,4 +3,4 @@ export const ServerAddress = new URL(document.URL)
 
 //To debug it change it
 // in case of any errors please verify that it's not caused by having this file in .gitignore!!!!
-export let MutServerAddress = new URL("http://localhost:80/")
+export let MutServerAddress = new URL(document.URL)
